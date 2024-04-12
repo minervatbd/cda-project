dfc541ce01456b9b15af99e76e99eb9656636c93
Minerva <minervatbd>
2024-04-12 16:33:53 -0400

    initial commit

---
+++ project.c
@@ -0,0 +1,128 @@
+#include "spimcore.h"
+
+/* ALU */
+void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
+{
+    if (ALUControl == 0)
+        *ALUresult = A + B;
+    else if (ALUControl == 1)
+        *ALUresult = A - B;
+    else if (ALUControl == 2)
+        *ALUresult = (int) A < (int) B ? 1 : 0;
+    else if (ALUControl == 3)
+        *ALUresult = A < B ? 1 : 0;
+    else if (ALUControl == 4)
+        *ALUresult = A & B;
+    else if (ALUControl == 5)
+        *ALUresult = A | B;
+    else if (ALUControl == 6)
+        *ALUresult = B << 16;
+    else if (ALUControl == 7)
+        *ALUresult = ~A;
+
+    *Zero = *ALUresult == 0 ? 1 : 0;
+    return;
+}
+
+/* Instruction Fetch */
+/* 10 Points */
+int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
+{
+
+}
+
+/* Instruction Partition */
+/* 10 Points */
+void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
+{
+
+}
+
+/* Instruction Decode */
+/* 30 Points */
+int instruction_decode(unsigned op,struct_controls *controls)
+{
+
+}
+
+/* Read Register */
+/* 5 Points */
+void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
+{
+
+}
+
+/* Sign Extend */
+/* 10 Points */
+void sign_extend(unsigned offset,unsigned *extended_value)
+{
+    unsigned direction = ((1 << 15) & offset) >> 15;
+    if (direction) {
+
+    }
+    else printf("f");
+}
+
+/* ALU Operations */
+int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
+{
+	// Handle second parameter to ALU
+    unsigned B = ALUSrc == 1 ? extended_value : data2;
+
+    // Handle ALU control unit
+    if (ALUOp == 7) {
+        // Add
+        if (funct == 0b100000)
+            ALUOp = 0;
+        // Subtract
+        else if (funct == 0b100010)
+            ALUOp = 1;
+        // And
+        else if (funct == 0b100100)
+            ALUOp = 4;
+        // Or
+        else if (funct == 0b100101)
+            ALUOp = 5;
+        // Slt
+        else if (funct == 0b101010)
+            ALUOp = 2;
+        // Sltu
+        else if (funct == 0b101011)
+            ALUOp = 3;
+        // Unknown funct code
+        else
+            return 1;
+    }
+
+    ALU(data1, B, ALUOp, ALUresult, Zero);
+    return 0;
+
+}
+
+/* Read / Write Memory */
+/* 10 Points */
+int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
+{
+
+}
+
+/* Write Register */
+/* 10 Points */
+void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
+{
+
+}
+
+/* PC Update */
+/* 15 Points */
+void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
+{
+
+}
+
+int main() {
+
+    sign_extend(0b11101010111001001110101011100100, NULL);
+    sign_extend(0b11101010111001000110101011100100, NULL);
+}
+
